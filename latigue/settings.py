3f45750de2665f3d78a9cdeba2782904a9c73654
aly <pymalien@gmail.com>
2024-06-17 04:55:56 +0200

    resolve static error

--- latigue/settings.py
+++ latigue/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'livereload.middleware.LiveReloadScript',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 
 ]
 
@@ -126,10 +127,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR / "staticfiles")]
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    # other static file directories...
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
